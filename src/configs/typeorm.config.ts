b72c0b4b3eab0fe2696ccb692abb129e10de12b2
신지훈 <sinjihun895@gmail.com>
2026-01-14 18:30:55 +0900

    feat: Add TypeORM configuration to AppModule

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,6 +1,6 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
+export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: 'localhost',
   port: 5432,
