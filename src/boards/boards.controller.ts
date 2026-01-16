c129bc29e4574cd2c3e45e328fe05fb84986d70c
신지훈 <sinjihun895@gmail.com>
2026-01-16 17:43:13 +0900

    커밋

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -10,40 +10,40 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import type { Board, BoardStatus } from './board.model';
+import type { BoardStatus } from './board.status.enum';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
-  // dddddddddddddddddddddddddddd
-  @Get('/')
-  getAllBoard(): Board[] {
-    return this.boardsService.getAllBoards();
-  }
 
-  @Post()
-  @UsePipes(ValidationPipe)
-  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
-    return this.boardsService.createBoard(createBoardDto);
-  }
+  // @Get('/')
+  // getAllBoard(): Board[] {
+  //   return this.boardsService.getAllBoards();
+  // }
 
-  @Get('/:id')
-  getBoardById(@Param('id') id: string): Board | undefined {
-    return this.boardsService.getBoardById(id);
-  }
+  // @Post()
+  // @UsePipes(ValidationPipe)
+  // createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+  //   return this.boardsService.createBoard(createBoardDto);
+  // }
 
-  @Delete('/:id')
-  deleteBoardById(@Param('id') id: string): void {
-    this.boardsService.deleteBoard(id);
-  }
+  // @Get('/:id')
+  // getBoardById(@Param('id') id: string): Board | undefined {
+  //   return this.boardsService.getBoardById(id);
+  // }
 
-  @Patch('/:id/status')
-  updateBoardStatus(
-    @Param('id') id: string,
-    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
-  ): Board | undefined {
-    return this.boardsService.updateBoardStatus(id, status);
-  }
+  // @Delete('/:id')
+  // deleteBoardById(@Param('id') id: string): void {
+  //   this.boardsService.deleteBoard(id);
+  // }
+
+  // @Patch('/:id/status')
+  // updateBoardStatus(
+  //   @Param('id') id: string,
+  //   @Body('status', BoardStatusValidationPipe) status: BoardStatus,
+  // ): Board | undefined {
+  //   return this.boardsService.updateBoardStatus(id, status);
+  // }
 }
